b46361ebc8fb43e9796613d0ddecc6ffef1d6bd5
Kelvin Ajayi <68742854+scryptwiz@users.noreply.github.com>
2025-02-05 08:59:46 +0100

    fix: fixed type definition bug on deployment

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -2,5 +2,5 @@ import { Preloaded } from "convex/react"
 import { api } from "../../convex/_generated/api"
 
 export type preloadedDocProps = {
-	preloadedDoc: Preloaded<typeof api.documents.getById>
+	preloadedDoc: Preloaded<typeof api.documents.getDocById>
 }
